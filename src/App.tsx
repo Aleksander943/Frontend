0f8e8540ceb5a21a986383e0aa58b590a3e25dde
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 18:57:49 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./components/login";
-import {Dashboard} from "./components/Dashboard";
+
 import { Register } from "./components/Register";
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
       <div className="bg-gray-200 min-h-screen w-full flex items-center justify-center">
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Register/>} />
+         
           {/* Redireciona rotas inexistentes para o login */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
