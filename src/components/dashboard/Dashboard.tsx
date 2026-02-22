f35aaede1c83154e5d0b777d8ab2a7b82c82a7a0
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-22 16:41:44 -0300

    style: tenho da tela

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ import { Transaction } from "./transaction/transaction"
 
 export function Dashboard() {
 return (
-   <div className="w-full max-w-3xl rounded-[28px] bg-white shadow-[0_2px_8px_rgba(0,0,0,0.06),0_16px_40px_rgba(0,0,0,0.08)]">
+   <div className="w-full h-screen max-w-3xl rounded-[28px] bg-white shadow-[0_2px_8px_rgba(0,0,0,0.06),0_16px_40px_rgba(0,0,0,0.08)]">
    <Home />
    <Transaction />
    <NavBar />
